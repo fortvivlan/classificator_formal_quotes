c7800d0445191bafeb9fa65b53f1eaf0727273fc
fortvivlan <71529585+fortvivlan@users.noreply.github.com>
2021-04-19 12:31:07 +0300

    Update topictilingver2.py

--- topictilingver2.py
+++ topictilingver2.py
@@ -1,5 +1,6 @@
 import pickle
 import json
+import os
 
 from numpy.linalg import norm
 from numpy import dot
@@ -19,20 +20,21 @@ def create_vector(win, ntops):
 
 def load_data():
     with open('/home/al/Documents/PythonFiles/files/disser/readydata/partial.json', 'r', encoding='utf8') as file:
-        data = json.load(file)[:2000]
+        data = json.load(file)[:100]
     return data
 
 
 def movement(doc, winsize, ntops):
-    if len(doc) < winsize:
+    if len(doc) <= winsize:
         # print(f'Some doc has less than {winsize} sents!')
         return
     vecs = []
     start = 0
     end = winsize
+    # end = 1
     for i in range(len(doc) - winsize):
         vecs.append(create_vector(doc[start:end], ntops))
-        vecs.append(create_vector(doc[end:winsize], ntops))
+        vecs.append(create_vector(doc[end:end + winsize], ntops))
         start += 1
         end += 1
     return vecs
@@ -46,44 +48,84 @@ def cosine(vecs):
 
 
 def splitter(cos, doc, winsize):
-    minimum = cos.index(min(cos))
-    crack = (minimum + 1) * winsize
-    doc1 = doc[:crack + 1]
-    doc2 = doc[crack + 1:]
-    return [doc1, doc2]
+    segmented = []
+    mean = sum(cos) / len(cos)
+    sigma = (sum((x - mean) ** 2 for x in cos) / len(cos)) ** 0.5
+    threshold = mean - sigma / 2
+    breakerpoints = []
+    for i in range(len(cos)):
+        hl = cos[i]
+        j = i
+        while True:
+            if j == 0:
+                break
+            if cos[j - 1] > cos[j]:
+                hl = cos[j - 1]
+                j -= 1
+            else:
+                break
+        hr = cos[i]
+        r = i
+        while True:
+            if r == len(cos) - 1:
+                break
+            if cos[r + 1] > cos[j]:
+                hr = cos[r + 1]
+                r += 1
+            else:
+                break
+        dscore = 0.5 * (hl + hr - 2 * cos[i])
+        if dscore > threshold:
+            breakerpoints.append(i + winsize)
+    if not breakerpoints:
+        return None
+    starter = 0
+    for index in breakerpoints:
+        if index != starter:
+            segmented.append(doc[starter:index])
+        starter = index
+    if starter != len(doc) - 1:
+        segmented.append(doc[starter:])
+    return segmented
 
 
 print('Loading LDA model...')
-ldamodel = pickle.load(open('/home/al/Documents/PythonFiles/files/disser/readydata/20k/lemmatized/LDA', 'rb'))
+ldamodel = pickle.load(open('/home/al/Documents/PythonFiles/files/disser/readydata/20k/lemmatized/LDAver', 'rb'))
 print('Loading data...')
 dataset = load_data()
 windowsize = int(input('Window size: '))
 topics = int(input('Quantity of topics: '))
 print('Calculating vectors and cosine distances...')
-finals = []
 num = 1
 lost = 0
-finale = open('/home/al/Documents/PythonFiles/files/disser/readydata/testsplitted.txt', 'a', encoding='utf8')
+if os.path.exists('/home/al/Documents/PythonFiles/files/disser/readydata/testsplitpuncstop.txt'):
+    os.remove('/home/al/Documents/PythonFiles/files/disser/readydata/testsplitpuncstop.txt')
+finale = open('/home/al/Documents/PythonFiles/files/disser/readydata/testsplitpuncstop.txt', 'a', encoding='utf8')
 with open('/home/al/Documents/PythonFiles/files/disser/readydata/partial_clean3.json', 'r', encoding='utf8') as jison:
     original = json.load(jison)
 for k in range(len(dataset)):
     vectors = movement(dataset[k], windowsize, topics)
     if vectors:
         cosines = cosine(vectors)
-        if not cosines:
-            print(f'Text No {num} hasn\'t got enough sents!')
-            lost += 1
+        print(original[k], file=finale)
+        print('_____', file=finale)
+        # print('Vector:', vectors)
+        # print('Cosines: ', cosines)
+        print(cosines, file=finale)
+        res = splitter(cosines, dataset[k], windowsize)
+        if res:
+            print(*res, sep='\n\n', end='\n\n\n', file=finale)
         else:
-            print(original[k], file=finale)
-            print('_____', file=finale)
-            print(*splitter(cosines, dataset[k], windowsize), sep='\n\n', end='\n\n\n', file=finale)
-            print('~' * 10, file=finale)
-            # finals.append(splitter(cosines, document, windowsize))
+            print('ZERO BREAKPOINTS FOUND', file=finale)
+        # if splitter:
+        #     print('Result:')
+        #     print(*splitter(cosines, dataset[k], windowsize), sep='\n\n', end='\n\n\n')
+        # else:
+        #     print('Couldn\'t split text!')
+        print('~' * 10, file=finale)
+    else:
+        # print(f'Text No {num} hasn\'t got enough sents!')
+        lost += 1
     num += 1
 print(f'{lost} texts shorter than windowsize')
-print('Done. Writing to file...')
 finale.close()
-# with open('/home/al/Documents/PythonFiles/files/disser/readydata/testsplitted.txt', 'w', encoding='utf8') as finale:
-#     for elem in finals:
-#         print(*elem, sep='\n\n', end='\n\n\n', file=finale)
-#         print('~' * 10, file=finale)
