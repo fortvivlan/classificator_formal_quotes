d55452a32589afa867c8ec436122984a49e9a26d
fortvivlan <71529585+fortvivlan@users.noreply.github.com>
2021-05-06 19:36:30 +0300

    Update topictilingver2.py

--- topictilingver2.py
+++ topictilingver2.py
@@ -2,11 +2,12 @@ import pickle
 import json
 import os
 
+from morphoclass import MorphoToken
 from numpy.linalg import norm
 from numpy import dot
 
 
-def create_vector(win, ntops):
+def create_vector(win, ntops, ldamodel):
     """ Creates a vector for a window span. Ntops - number of topics """
     vec = [0] * ntops
     wintoks = []
@@ -20,12 +21,27 @@ def create_vector(win, ntops):
 
 def load_data():
     """ Loads data... suddenly """
-    with open(r'F:\CODE\files\disser\readydata\partial.json', 'r', encoding='utf8') as file:  # no ready json
-        data = json.load(file)[:100]  # size restricted for testing purposes
-    return data
+    datafull = pickle.load(open('/home/al/PythonFiles/files/disser/readydata/morpho/PARTIAL_LJ', 'rb'))[:100]
+    data = []
+    datatexts = []
+    for doc in datafull:
+        docnew = []
+        doctext = []
+        for sent in doc:
+            sentlemmas = []
+            senttext = []
+            for token in sent:
+                sentlemmas.append(token.lemma)
+                senttext.append(token.form)
+            docnew.append(sentlemmas)
+            doctext.append(senttext)
+        data.append(docnew)
+        datatexts.append(doctext)
+    del datafull
+    return data, datatexts
 
 
-def movement(doc, winsize, ntops):
+def movement(doc, winsize, ntops, ldamodel):
     """ Calculates vectors for sents winsize, sort of sent 1-2 * 3-4, then 2-3 * 4-5 and so on. Be careful:
     first span of a winsize isn't being split, so you can't get a split between 1st and 2nd sents if your
     winsize is 2 """
@@ -37,8 +53,8 @@ def movement(doc, winsize, ntops):
     # end = 1 - to get this to work you'd have to make several smart changes
     for i in range(len(doc) - winsize):
         # This way we have a list twice the size of intervals, so that calculating cosines we'd go with step = 2
-        vecs.append(create_vector(doc[start:end], ntops))
-        vecs.append(create_vector(doc[end:end + winsize], ntops))
+        vecs.append(create_vector(doc[start:end], ntops, ldamodel))
+        vecs.append(create_vector(doc[end:end + winsize], ntops, ldamodel))
         start += 1
         end += 1
     return vecs
@@ -57,7 +73,7 @@ def splitter(cos, doc, winsize):
     segmented = []
     mean = sum(cos) / len(cos)
     sigma = (sum((x - mean) ** 2 for x in cos) / len(cos)) ** 0.5
-    threshold = mean - sigma / 2  
+    threshold = mean - sigma / 2
     # d-scores less than threshold are ok: d-score for a local max is 0, the greater d-score, the deeper local min
     breakerpoints = []
     for i in range(len(cos)):
@@ -67,8 +83,8 @@ def splitter(cos, doc, winsize):
         while True:
             if j == 0:
                 break
-            if cos[j - 1] > cos[j]:  
-                # we break if previous cosine equals current. 
+            if cos[j - 1] > cos[j]:
+                # we break if previous cosine equals current.
                 # Flats are not considered, this way we lower d-scores for sequences of zeros
                 hl = cos[j - 1]
                 j -= 1
@@ -89,7 +105,7 @@ def splitter(cos, doc, winsize):
         if dscore > threshold:
             breakerpoints.append(i + winsize)  # the algorithm works so that index + winsize gives the sent we need
     if not breakerpoints:  # no d-score appeared high enough to pass the threshold
-        return None
+        return
     # Segmenting work goes here
     starter = 0
     for index in breakerpoints:
@@ -98,36 +114,49 @@ def splitter(cos, doc, winsize):
         starter = index
     if doc[starter:]:  # considers tail
         segmented.append(doc[starter:])
-    return segmented
+    return segmented, breakerpoints
 
 
-print('Loading LDA model...')
-ldamodel = pickle.load(open(r'F:\CODE\files\disser\LDAs\LDAmodel', 'rb'))  # LDA MODEL LOAD check it if you change model
-print('Loading data...')
-dataset = load_data()
-windowsize = int(input('Window size: '))
-topics = int(input('Quantity of topics: '))  # I guess I could code it into the model but I was too lazy
-print('Calculating vectors and cosine distances...')
-lost = 0  # to count texts lost due to their incoherent length
-pathtores = r'F:\CODE\files\disser\readydata\testsplitpuncstop.txt'  # path to resulting text file
-if os.path.exists(pathtores):  # to prevent script from endlessly adding to file
-    os.remove(pathtores)
-finale = open(pathtores, 'a', encoding='utf8')
-with open(r'F:\CODE\files\disser\readydata\partial_clean3.json', 'r', encoding='utf8') as jison:
-    original = json.load(jison)
-for k in range(len(dataset)):
-    vectors = movement(dataset[k], windowsize, topics)
-    if vectors:
-        cosines = cosine(vectors)
-        print(original[k], file=finale)
-        print('_____', file=finale)
-        res = splitter(cosines, dataset[k], windowsize)
-        if res:
-            print(*res, sep='\n\n', end='\n\n\n', file=finale)
+def topictiling():
+    p = '/home/al/PythonFiles/files/disser/LDAs/'
+    no = input('Experiment name: ')  # for file naming
+    filenames = [name for name in os.listdir(p) if not os.path.splitext(name)[1] or os.path.isdir(name)]
+    print('Your models are as follows:')
+    print(*filenames)
+    modelname = input('What do we use?\n')
+    print('Loading LDA model...')  # LDA MODEL LOAD check it if you change model
+    ldamodel = pickle.load(open(os.path.join(p, modelname), 'rb'))
+    print('Loading data...')
+    dataset, rawtext = load_data()
+    windowsize = int(input('Window size: '))
+    topics = int(input('Quantity of topics: '))  # I guess I could code it into the model but I was too lazy
+    print('Calculating vectors and cosine distances...')
+    lost = 0  # to count texts lost due to their incoherent length
+    breakpoints = {}
+    pathtores = f'/home/al/PythonFiles/files/disser/experiments/experiment_{no}.txt'  # path to resulting text file
+    if os.path.exists(pathtores):  # to prevent script from endlessly adding to file
+        os.remove(pathtores)
+    finale = open(pathtores, 'a', encoding='utf8')
+    for k in range(len(dataset)):
+        vectors = movement(dataset[k], windowsize, topics, ldamodel)
+        if vectors:
+            cosines = cosine(vectors)
+            res = splitter(cosines, rawtext[k], windowsize)
+            if res:
+                for part in res[0]:
+                    for sent in part:
+                        print(*sent, file=finale)
+                    print('_____________', file=finale)
+                breakpoints[k] = res[1]
+            else:
+                print('ZERO BREAKPOINTS FOUND', file=finale)
+            print('~' * 10, file=finale)
         else:
-            print('ZERO BREAKPOINTS FOUND', file=finale)
-        print('~' * 10, file=finale)
-    else:
-        lost += 1
-print(f'{lost} texts shorter than windowsize')
-finale.close()
+            lost += 1
+    print(f'{lost} texts shorter than windowsize')
+    finale.close()
+    pickle.dump(breakpoints, open(f'/home/al/PythonFiles/files/disser/experiments/breakpoints_{no}', 'wb'))
+
+
+if __name__ == '__main__':
+    topictiling()
